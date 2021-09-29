812ed968cd9a26e27bb796438de8bbd95ab63b0e
avolgin <andrey.volgin@t-systems.com>
2021-09-29 19:51:49 +0300

    implement function sumSalary 2-module-1-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
   // ваш код...
+  let amount = 0;
+  for (let n in salaries) {
+    if (typeof salaries[n] === "number" && !isNaN(salaries[n]) &&
+               salaries[n] != Infinity && salaries[n] != -Infinity ) {
+	    amount += salaries[n];
+    }
+ }
+ return amount;
 }
