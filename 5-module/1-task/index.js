e8d8ae284ae2a0b0d7f4466fbc64c6031cc64af0
avolgin <andrey.volgin@t-systems.com>
2021-10-11 16:45:06 +0300

    implemented tasks 1 and 2 from 5-module

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,5 @@
 function hideSelf() {
   // ваш код...
+  let elem = document.querySelector('.hide-self-button');
+  elem.addEventListener('click', () => {elem.hidden = true});
 }
--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,5 @@
 function toggleText() {
   // ваш код...
+  let elem = document.querySelector('.toggle-text-button');
+  elem.addEventListener('click', () => {text.hidden = !text.hidden;});
 }
