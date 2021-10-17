e38088e15808c067104a0696140b6eba832a3771
avolgin <andrey.volgin@t-systems.com>
2021-10-17 15:23:27 +0300

    implemented tasks 3 from 5-module and all 3 tasks from 6-module

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,32 @@
 function initCarousel() {
   // ваш код...
+
+  const maxCounter = 3;
+  const minCounter = 0;
+
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+
+  const innerSlide   = document.querySelector('.carousel__inner');
+  const stepSize = innerSlide.offsetWidth;
+
+  let slideCounter = minCounter;
+  arrowLeft.style.display = 'none';
+
+  arrowRight.addEventListener('click', () => {
+    ++slideCounter;
+    innerSlide.style.transform = 'translateX(-' + (stepSize*slideCounter) + 'px)';
+    if (slideCounter === maxCounter) arrowRight.style.display = 'none';
+    arrowLeft.style.display = '';  
+  });
+
+  arrowLeft.addEventListener('click', () => {
+    --slideCounter;
+    innerSlide.style.transform = 'translateX(-' + (stepSize*(slideCounter)) + 'px)';
+    if (slideCounter === (minCounter)) arrowLeft.style.display = 'none';  
+    arrowRight.style.display = '';
+  });
+
+  
+
 }
