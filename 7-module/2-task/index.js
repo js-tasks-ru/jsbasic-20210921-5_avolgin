9d24a370143d78b42f9be583fc9c5057a80a2604
avolgin <andrey.volgin@t-systems.com>
2021-10-22 01:01:50 +0300

    fixed task 2 from 7-module

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -4,33 +4,27 @@ export default class Modal {
   #modaltitle;
   #modalBody;
   
-
   constructor() {
 
-    document.addEventListener('keydown', event => {
-      if ((event.code === 'Escape') && 
-         document.body.classList.contains('is-modal-open')) {
-        this.close();
-      }
-    });
+    this.render();
 
   }
 
   setTitle(value) {
-    //const modalTitle = document.querySelector('.modal__title');
-    //modalTitle.textContent = value;
+    const modalTitle = this.elem.querySelector('.modal__title');
+    modalTitle.textContent = value;
    this.#modaltitle = value;
   }
 
   setBody(value) {
-    //const modalBody = document.querySelector('.modal__body');
-    //modalBody.append(value);
+    
+    const modalBody = this.elem.querySelector('.modal__body');
+    modalBody.append(value);
     this.#modalBody = value;
   }
-  
-  open() {
 
-    const modalElem = createElement(`
+  render() {
+    this.elem = createElement(`
       <div class="modal">
         <div class="modal__overlay"></div>
         <div class="modal__inner">
@@ -44,21 +38,31 @@ export default class Modal {
         </div>
       </div>  
     `);
-
-    const nodeBody = modalElem.querySelector('.modal__body');
+  }
+  
+  open() {
+    
+    const nodeBody = this.elem.querySelector('.modal__body');
     nodeBody.append(this.#modalBody);
 
-    const modalClose  = modalElem.querySelector('.modal__close');
+    const modalClose  = this.elem.querySelector('.modal__close');
     modalClose.addEventListener('click', () => {
       this.close(); 
     });
 
-    document.body.querySelector('.container').prepend(modalElem);
+    document.addEventListener('keydown', event => {
+      if ((event.code === 'Escape') && 
+         document.body.classList.contains('is-modal-open')) {
+        this.closeDoc();
+      }
+    });
+
+    document.body.append(this.elem);
     document.body.classList.add('is-modal-open');
   }
 
   close(){
-    const elem = document.querySelector('.modal');
+    const elem = this.elem;
     elem.remove();
     document.body.classList.remove('is-modal-open');
     document.addEventListener('keydown', event => {
@@ -67,9 +71,19 @@ export default class Modal {
         this.close();
       }
     });
-
   }
 
+  closeDoc(){
+    const elem = document.body.querySelector('.modal');
+    elem.remove();
+    document.body.classList.remove('is-modal-open');
+    document.addEventListener('keydown', event => {
+      if ((event.code === 'Escape') && 
+         document.body.classList.contains('is-modal-open')) {
+        this.close();
+      }
+    });
+  }
 
 
 }
