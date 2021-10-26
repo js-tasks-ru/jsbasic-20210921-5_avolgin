a1d7eb6d219156cb31fbce9f88abfa00b2fc013d
avolgin <andrey.volgin@t-systems.com>
2021-10-26 18:15:33 +0300

    implemented tasks 3 and 4 from 7-module

--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,73 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class StepSlider {
+  #stpPercent;
+  #sldPercent;
   constructor({ steps, value = 0 }) {
+    this.steps = steps;
+    this.value = value;
+    this.#stpPercent = Math.round(100/(steps-1));
+    this.#sldPercent = this.#stpPercent*this.value;
+    this.render();   
+  }
+
+  render() {
+    this.elem = createElement(`
+      <div class="slider">
+       <!--Ползунок слайдера с активным значением-->
+       <div class="slider__thumb" style="left: ${this.#sldPercent}%;">
+        <span class="slider__value">${this.value}</span>
+       </div>
+        <!--Заполненная часть слайдера-->
+        <div class="slider__progress" style="width: ${this.#sldPercent}%;"></div>
+        <!--Шаги слайдера-->
+        <div class="slider__steps"></div>
+      </div>
+    `);
+
+    const sSteps = this.elem.querySelector('.slider__steps'); 
+
+    //add slider steps
+    for (let i = 0; i < this.steps; i++) {
+     let iSteps;
+     if (i==this.value) {
+       iSteps = createElement(`
+       <span class="slider__step-active"></span>
+        `);
+      } else {
+        iSteps = createElement(`
+        <span></span>
+        `);
+      };
+      sSteps.append(iSteps);  
+    }
+
+    this.elem.addEventListener('click', (event) => {
+     let leftPx = event.clientX - this.elem.getBoundingClientRect().left;
+     this.value = Math.round((leftPx / this.elem.offsetWidth) * (this.steps-1));
+     this.#sldPercent = this.#stpPercent*this.value;
+      let thumb = this.elem.querySelector('.slider__thumb');
+      let progress = this.elem.querySelector('.slider__progress');
+      thumb.style.left = `${this.#sldPercent }%`;
+      progress.style.width = `${this.#sldPercent}%`;
+      let sldValue = this.elem.querySelector('.slider__value');
+      sldValue.textContent = this.value;
+      let sldSpan = (this.elem.querySelector('.slider__steps')).querySelectorAll('span');
+
+      for (let i = 0; i < sldSpan.length; i++) {
+        if (sldSpan[i].classList.contains('slider__step-active')) {
+          sldSpan[i].classList.remove('slider__step-active');     
+        };
+        if (i==this.value) {
+          sldSpan[i].classList.add('slider__step-active');     
+        }; 
+      }
+
+      const custEvent = new CustomEvent('slider-change', {
+        detail: this.value,
+        bubbles: true
+      });
+      this.elem.dispatchEvent(custEvent);
+    });
   }
 }
--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -1,4 +1,142 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class StepSlider {
+  #stpPercent;
+  #sldPercent;
   constructor({ steps, value = 0 }) {
+    this.steps = steps;
+    this.value = value;
+    this.#stpPercent = Math.round(100/(steps-1));
+    this.#sldPercent = this.#stpPercent*this.value;
+    this.render();
   }
+
+  render() {
+    this.elem = createElement(`
+      <div class="slider">
+       <!--Ползунок слайдера с активным значением-->
+       <div class="slider__thumb" style="left: ${this.#sldPercent}%;">
+        <span class="slider__value">${this.value}</span>
+       </div>
+        <!--Заполненная часть слайдера-->
+        <div class="slider__progress" style="width: ${this.#sldPercent}%;"></div>
+        <!--Шаги слайдера-->
+        <div class="slider__steps"></div>
+      </div>
+    `);
+
+    let thumb = this.elem.querySelector('.slider__thumb');
+
+    thumb.ondragstart = function() {
+      return false;
+    };
+
+    const sSteps = this.elem.querySelector('.slider__steps'); 
+    let thSteps = this.steps;
+
+    //add slider steps
+    for (let i = 0; i < this.steps; i++) {
+     let iSteps;
+     if (i==this.value) {
+       iSteps = createElement(`
+       <span class="slider__step-active"></span>
+        `);
+      } else {
+        iSteps = createElement(`
+        <span></span>
+        `);
+      };
+      sSteps.append(iSteps);  
+    }
+
+    this.elem.addEventListener('click', (event) => {
+      let leftPx = event.clientX - this.elem.getBoundingClientRect().left;
+      this.value = Math.round((leftPx / this.elem.offsetWidth) * (this.steps-1));
+      this.#sldPercent = this.#stpPercent*this.value;
+      let thumb = this.elem.querySelector('.slider__thumb');
+      let progress = this.elem.querySelector('.slider__progress');
+      thumb.style.left = `${this.#sldPercent }%`;
+      progress.style.width = `${this.#sldPercent}%`;
+      let sldValue = this.elem.querySelector('.slider__value');
+      sldValue.textContent = this.value;
+      let sldSpan = (this.elem.querySelector('.slider__steps')).querySelectorAll('span');
+
+      for (let i = 0; i < sldSpan.length; i++) {
+        if (sldSpan[i].classList.contains('slider__step-active')) {
+          sldSpan[i].classList.remove('slider__step-active');     
+        };
+        if (i==this.value) {
+          sldSpan[i].classList.add('slider__step-active');     
+        }; 
+      }
+
+      const custEvent = new CustomEvent('slider-change', {
+        detail: this.value,
+        bubbles: true
+      });
+      this.elem.dispatchEvent(custEvent);
+    });
+
+    thumb.addEventListener('pointerdown', (event) => {
+      this.elem.classList.add('slider_dragging');
+      
+      document.addEventListener('pointermove', onMouseMove);
+
+      function onMouseMove(event) {
+        let elem = document.body.querySelector('.slider');
+        let left = event.clientX - elem.getBoundingClientRect().left;
+        let leftRelative = left / elem.offsetWidth;
+
+        if (leftRelative < 0) {
+            leftRelative = 0;
+        }
+
+        if (leftRelative > 1) {
+              leftRelative = 1;
+        }
+
+        let leftPercents = leftRelative * 100;
+
+        let thumb = elem.querySelector('.slider__thumb');
+        let progress = elem.querySelector('.slider__progress');
+
+        thumb.style.left = `${leftPercents}%`;
+        progress.style.width = `${leftPercents}%`;
+
+        
+        let approximateValue = leftRelative * (thSteps - 1);
+        this.value = Math.round(approximateValue);
+        let sldValue = elem.querySelector('.slider__value');
+        sldValue.textContent = this.value;
+
+        let sldSpan = (elem.querySelector('.slider__steps')).querySelectorAll('span');
+        for (let i = 0; i < sldSpan.length; i++) {
+          if (sldSpan[i].classList.contains('slider__step-active')) {
+            sldSpan[i].classList.remove('slider__step-active');     
+          };
+          if (i==this.value) {
+            sldSpan[i].classList.add('slider__step-active');     
+          }; 
+        }
+
+      };
+      
+      document.addEventListener('pointerup', onMouseUp);
+      
+      function onMouseUp(event) {
+      let elem = document.body.querySelector('.slider'); 
+       document.removeEventListener('pointermove', onMouseMove);
+        elem.classList.remove('slider_dragging');
+        const custEvent = new CustomEvent('slider-change', {
+          detail: this.value,
+          bubbles: true
+        });
+        elem.dispatchEvent(custEvent);
+      };
+
+
+    });
+
+  }
+
 }
